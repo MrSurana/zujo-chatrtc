b1807e2dc4899bf5b5d77613fb9601757a0e27c1
Vaibhav Surana <mrsurana@tabdevelopers.in>
2020-08-10 21:38:04 +0530

    added peerjs for webrtc peer to peer chat

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,7 +9,8 @@ var app = new Vue({
         loggedInUser: {
             _id: '1',
             name: 'johndoe',
-        }
+        },
+        peer: null
     },
 
     mounted: function () {
@@ -17,6 +18,29 @@ var app = new Vue({
         this.fetchUsers();
 
         // TODO: Ask for username if not logged in
+        this.loggedInUser._id = prompt('userid');
+
+        // init Peer and init websocket
+        this.peer = new Peer(this.loggedInUser._id, { host: 'localhost', port: 9000, path: '/chatrtc' });
+
+        this.peer.on('open', (id) => {
+            // console.log('My peer ID is: ' + id);
+        });
+
+        this.peer.on('connection', (conn) => {
+            console.log('Connected to: %s', conn.peer);
+
+            const userIndex = this.users.findIndex(u => u._id == conn.peer);
+            if (userIndex > -1) {
+                this.users[userIndex].conn = conn;
+            }
+
+            conn.on('data', (data) => {
+                if (this.selUser && data.data.senderId == this.selUser._id) {
+                    this.messages.push(data.data)
+                }
+            });
+        });
     },
 
     methods: {
@@ -26,10 +50,23 @@ var app = new Vue({
             // Fetch user messages
             this.fetchMessages(user._id);
 
-            // TODO: Check if connection is active with user, if not then try connecting
+            // Check if connection is active with user, if not then try connecting
+            if (!user.conn || !user.conn.open) {
+                user.conn = this.peer.connect(user._id);
+
+                user.conn.on('data', (data) => {
+                    if (this.selUser && data.data.senderId == this.selUser._id) {
+                        this.messages.push(data.data)
+                    }
+                });
+            }
         },
 
         sendMessage: function () {
+            this.selUser.conn.send({
+                type: 'message',
+                data: { _id: this.messages.length + 1, text: this.message, senderId: this.loggedInUser._id }
+            });
             this.messages.push({ _id: this.messages.length + 1, text: this.message, senderId: this.loggedInUser._id });
             this.message = '';
         },
